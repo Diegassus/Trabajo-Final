7b58c6068214f20c7d7b37be0b4a6005cba939f4
Diegassus <diegomdiaz53@gmail.com>
2022-05-18 22:52:45 -0300

    2

--- main.js
+++ main.js
@@ -8,17 +8,7 @@ slider[4] = "https://www.hrkgame.com/media/screens/expansion-hearts-of-iron-iv-l
 slider[5] = "https://i0.wp.com/caniplaythat.com/wp-content/uploads/2021/05/uncharted-4-accessibility-stats.jpg?resize=800%2C450&ssl=1"; //uncharted 4
 slider[6] = "https://images.pushsquare.com/8b917a36c5c4b/horizon-zero-dawn-ps4-1.large.jpg"; //horizon zd
 
-///
-var titulo= new Array;
-var texto= new Array;
-
-titulo[1]=document.createElement('p');
-texto[1]=document.createTextNode("Ambientado en el siglo IX, el juego tiene lugar durante la invasión de Gran Bretaña por parte de los Vikingos, con el jugador controlando al guerrero vikingo Eivor dirigiendo a su pueblo contra el rey sajón Alfredo el Grande y sus reinos vasallos, a los que controla a través de una secta pagana llamada la Orden de los Antiguos, y que pretende cristianizarla. ");//acv
-titulo[1].appendChild(texto);
-///var id =document.getElementById('padre')= titulo[index];
-  ///  padre.appendChild(titulo[index]);
 
 function mostrar(index){
     document.slide.src=slider[index];
-    
 };
